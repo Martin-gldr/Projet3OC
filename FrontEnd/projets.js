42a8ceacf8d998afe0381894a0a80ec8d92460e4
Martin-gldr <martin.glandier@gmail.com>
2025-04-09 14:41:48 +0200

    mode edition affichage

--- FrontEnd/projets.js
+++ FrontEnd/projets.js
@@ -5,6 +5,19 @@ const works = await reponse.json();
 const reponseCategories = await fetch("http://localhost:5678/api/categories")
 const categories = await reponseCategories.json();
 console.log(categories)
+
+
+const userIn = window.localStorage.getItem("token")
+console.log(userIn)
+// login ou logout 
+const loginLink = document.getElementById("loginLink")
+const logoutLink = document.getElementById("logoutLink")
+// affichage mode edition 
+const headerEdition = document.querySelector(".headerEdition")
+const ModifierLink = document.querySelector(".editionMesProjets a")
+
+
+
 // creation des cards des projet 
 function genererWorks (works){
     for(let i = 0; i< works.length; i++){
@@ -27,55 +40,70 @@ genererWorks(works)
 
 
 
-// CATEGORIES
+if (userIn === null ){
 
-// ajout catégorie "tous"
-const divCategories = document.querySelector(".categories");
-const btnTous = document.createElement("button");
-btnTous.innerText ="Tous"
-btnTous.classList.add("btnHomePage")
-btnTous.id = 0
 
-divCategories.appendChild(btnTous)
+    loginLink.classList.remove("hide")
+    logoutLink.classList.add("hide")
+    headerEdition.classList.add("hide")
+    ModifierLink.classList.add("hide")
 
-// creation des boutons catégories et filtre
+    // CATEGORIES
 
-for(let i = 0; i<categories.length;i++){
-    const divCategories = document.querySelector(".categories");
-    const BtnElement = document.createElement("button");
-    BtnElement.innerText = categories[i].name;
-    BtnElement.id = categories[i].name;
-    BtnElement.classList.add("btnHomePage");
-    divCategories.appendChild(BtnElement);
-    
-    BtnElement.addEventListener("click",()=>{
-   const worksFiltree = works.filter(function(work){
-        return work.categoryId === categories[i].id });
-        document.querySelector(".gallery").innerHTML="";
-        genererWorks(worksFiltree);
-        let btnliste = document.querySelectorAll("button");
-        for(let i=0; i<btnliste.length;i++){
-                btnliste[i].classList.remove("btnSelected")
-        };
-        BtnElement.classList.toggle("btnSelected")
+        // ajout catégorie "tous"
+        const divCategories = document.querySelector(".categories");
+        const btnTous = document.createElement("button");
+        btnTous.innerText ="Tous"
+        btnTous.classList.add("btnHomePage")
+        btnTous.id = 0
+        divCategories.appendChild(btnTous)
+
+        // creation des boutons catégories et filtre
+        for(let i = 0; i<categories.length;i++){
+            const divCategories = document.querySelector(".categories");
+            const BtnElement = document.createElement("button");
+            BtnElement.innerText = categories[i].name;
+            BtnElement.id = categories[i].name;
+            BtnElement.classList.add("btnHomePage");
+            divCategories.appendChild(BtnElement);
+            
+            BtnElement.addEventListener("click",()=>{
+        const worksFiltree = works.filter(function(work){
+                return work.categoryId === categories[i].id });
+                document.querySelector(".gallery").innerHTML="";
+                genererWorks(worksFiltree);
+                let btnliste = document.querySelectorAll("button");
+                for(let i=0; i<btnliste.length;i++){
+                        btnliste[i].classList.remove("btnSelected")
+                };
+                BtnElement.classList.toggle("btnSelected")
+            })
+        }
+
+        btnTous.addEventListener("click",()=>{
+        const worksTous = works.filter(function(work){
+            return work
+        });
+            document.querySelector(".gallery").innerHTML="";
+            genererWorks(worksTous);
+            let btnliste = document.querySelectorAll("button");
+                for(let i=0; i<btnliste.length;i++){
+                        btnliste[i].classList.remove("btnSelected")
+                };
+            btnTous.classList.toggle("btnSelected")
+        }) 
+
+} else if(userIn != null){
+    ModifierLink.classList.remove("hide")
+    headerEdition.classList.remove("hide")
+    logoutLink.classList.remove("hide")
+    loginLink.classList.add("hide")
+    logoutLink.addEventListener("click",()=>{ 
+        window.localStorage.removeItem("token")
+        console.log(userIn)
+        location.href="./index.html"
     })
+
+
 }
 
-
-//btn tous 
-
-btnTous.addEventListener("click",()=>{
-   const worksTous = works.filter(function(work){
-    return work
-   });
-    document.querySelector(".gallery").innerHTML="";
-    genererWorks(worksTous);
-    let btnliste = document.querySelectorAll("button");
-        for(let i=0; i<btnliste.length;i++){
-                btnliste[i].classList.remove("btnSelected")
-        };
-    btnTous.classList.toggle("btnSelected")
-}) 
-
-const userIn = window.localStorage.getItem("token")
-console.log(userIn)
